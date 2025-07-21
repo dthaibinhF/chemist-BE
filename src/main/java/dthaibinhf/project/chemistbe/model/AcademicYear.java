16a8fa6d4f59319521620b763f8ed1c006335091
firane <dthaibinh03@gmail.com>
2025-07-21 17:48:27 +0700

    refactor(model): update AcademicYear entity and configuration
    Modify the AcademicYear entity to use escaped column name for "year".
    Change active profile in application.properties to 'dev'.
    Update allowed origins in SecurityConfig for CORS.

--- src/main/java/dthaibinhf/project/chemistbe/model/AcademicYear.java
+++ src/main/java/dthaibinhf/project/chemistbe/model/AcademicYear.java
@@ -1,6 +1,8 @@
 package dthaibinhf.project.chemistbe.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +11,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "academic_year")
 public class AcademicYear extends BaseEntity {
-    @Column(name = "year", nullable = false, length = 9)
+    @Column(name = "\"year\"", nullable = false, length = 9)
     private String year;
 
 }
